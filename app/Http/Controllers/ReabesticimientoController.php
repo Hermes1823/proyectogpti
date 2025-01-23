70afb34195538555b5148c1090d8321d1f6ab6e4
Stephano0207 <castanedastephano16@gmail.com>
2025-01-23 03:13:26 -0500

    Reabestecimiento

--- app/Http/Controllers/ReabesticimientoController.php
+++ app/Http/Controllers/ReabesticimientoController.php
@@ -2,20 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DetalleCompra;
 use App\Models\OrdenCompra;
+use App\Models\Producto;
 use App\Models\Proveedor;
 use Illuminate\Http\Request;
 
 class ReabesticimientoController extends Controller
 {
    public function index(){
-    $provedores= Proveedor::all();
-    return view("sistema.producto.reabestecimiento.index",compact("provedores"));
+    $proveedores= Proveedor::all();
+    return view("sistema.producto.reabestecimiento.index",compact("proveedores"));
    }
 
 
    public function store(Request $request){
 
+
+    // return dd($request);
+    $id_compra=$request->get("ordenCompra");
+    $ordenCompra= OrdenCompra::find($id_compra);
+    $ordenCompra->estado="RECIBIDA";
+    $ordenCompra->save();
+
+    $detalles= DetalleCompra::where("id_orden_compra","=",$id_compra);
+    foreach($detalles as $d){
+        $producto= Producto::find($d->id_producto);
+        $producto->cantidad=$producto->cantidad+$d->cantidad;
+        $producto->save();
+    }
+
+    session()->flash('message', 'registro exitoso');
+
+    return redirect()->route('reabesticimiento.index');
    }
 
    public function show($id){
@@ -29,9 +48,9 @@ class ReabesticimientoController extends Controller
 
    public function search_order_provee($id){
 
-    $order_provee = OrdenCompra::all()
-    ->where("ruc","=",$id)
-    ->where("estado","LIKE","%PENDIENTE%");
+    $order_provee = OrdenCompra::where("ruc","=",$id)
+    ->where("estado","LIKE","PENDIENTE")
+    ->get();
 
     if($order_provee){
         return response()
@@ -51,10 +70,10 @@ class ReabesticimientoController extends Controller
    }
 
    public function detalles_orden($id){
-    $orden_compra= OrdenCompra::find($id)->detalles();
+    $orden_compra= OrdenCompra::with("detalles.producto")->find($id);
     if($orden_compra){
-        return response()->
-        json(["data"=>$orden_compra,"success"=>true],200);
+        return response()
+        ->json(["data"=>$orden_compra,"success"=>true],200);
     }
     return response()->
     json(["data"=>null,"success"=>false],400);
--- resources/views/sistema/producto/reabestecimiento/index.blade.php
+++ resources/views/sistema/producto/reabestecimiento/index.blade.php
@@ -3,11 +3,107 @@
 @section('title', 'Reabesticimiento')
 
 @section('content_header')
-
+    Reabastecimiento
 @stop
 
 @section('content')
 
+
+    @if (session('message') == 'registro exitoso')
+        <x-adminlte-alert class="bg-teal text-uppercase" icon="fa fa-lg fa-thumbs-up" title="Correcto" dismissable>
+            Registro Exitoso
+        </x-adminlte-alert>
+
+        <script>
+            setTimeout(function() {
+                window.location.href = "{{ route('producto.index') }}";
+            }, 2000); // 2000 milisegundos = 2 segundos
+        </script>
+    @endif
+
+    <div class="card m-3">
+        <div class="card-body">
+
+         <form action="{{route('reabesticimiento.store')}}" method="post" id="formulario">
+            @csrf
+            <input type="hidden" name="estado" id="estado">
+            <div class="row">
+                <div class="col col-md-6 col-sm-12">
+                    <x-adminlte-select name="proveedor" label="Proveedores" label-class="text-lightblue"
+                        data-placeholder="Seleccione un proveedor" id="listaProveedores">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text bg-gradient-info">
+                                <i class="fas fa-user"></i>
+                            </div>
+                        </x-slot>
+                        <option selected>Selecciona un proveedor</option>
+                        @foreach ($proveedores as $proveedor)
+                            <option value="{{ $proveedor->ruc }}">{{ $proveedor->razon_social }}</option>
+                        @endforeach
+                    </x-adminlte-select>
+                </div>
+                <div class="col col-md-6 col-sm-12">
+                    <x-adminlte-select name="ordenCompra" label="Ordenes Compra" label-class="text-lightblue"
+                        data-placeholder="Seleccione una orden de compra" id="listaOrdenes">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text bg-gradient-info">
+                                <i class="fas fa-user"></i>
+                            </div>
+                        </x-slot>
+                        <option selected>Sin Ordenes de compra</option>
+
+                    </x-adminlte-select>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <button class="btn btn-success" type="button" id="btn_aprobar">Aprobar</button>
+                </div>
+
+                <div class="col">
+                    <button class="btn btn-danger" type="button" id="btn_rechazar">Rechazar</button>
+                </div>
+
+            </div>
+         </form>
+
+
+            <br>
+            <div class="container-fluid">
+
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Producto</th>
+                            <th scope="col">Cantidad </th>
+                            <th scope="col">Precio Unitario</th>
+                            <th scope="col">Importe</th>
+                        </tr>
+                    </thead>
+                    <tbody id="cuerpo_tabla"></tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="3" class="text-center">Total</td>
+                            <td>
+
+                                <input type="hidden" class="form-control" name="total" id="txtTotal"
+                                    aria-describedby="helpId" placeholder="" />
+
+                                <input type="number" class="form-control" id="txtTotal_" aria-describedby="helpId"
+                                    placeholder="" disabled=true />
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+
+            </form>
+        </div>
+
+
+
+    </div>
 @stop
 
 @section('css')
@@ -17,5 +113,5 @@
 
 @section('js')
 
-
+    <script src="{{ asset('js/reabesticimiento.js') }}"></script>
 @stop
