ef670e749243279d472f9dc5c921798127d4be6c
Edisson Galvez <edigalmor@outlook.com>
2025-01-31 00:09:30 -0500

    Fix ML again

--- public/aa/aaSales1Test.py
+++ public/aa/aaSales1Test.py
@@ -5,10 +5,10 @@ model = load('aaSales1.joblib')
 
 new_data = pd.DataFrame({
     'year': [2025],
-    'month': [1],
-    'day': [24],
-    'day_of_week': [2]
+    'month': [2],
+    'day': [1],
+    'day_of_week': [6]
 })
 
 predictions = model.predict(new_data)
-print(f"Predicción de demanda: {predictions[0]}")
+print(f"Predicción de demanda: {predictions[0]/32}")
